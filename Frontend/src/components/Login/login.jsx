e333f94a0cfde0adfd6796bc52fce8aa0d480c83
Kabil-ARCH <kabilmuthu777@gmail.com>
2025-02-02 12:25:37 +0530

    Made unwanted changes

--- Frontend/src/components/Login/login.jsx
+++ Frontend/src/components/Login/login.jsx
@@ -1,4 +1,11 @@
-import { Box, Container, Stack, TextField, Button, Typography } from "@mui/material";
+import {
+	Box,
+	Container,
+	Stack,
+	TextField,
+	Button,
+	Typography,
+} from "@mui/material";
 import google from "../../assets/g.jpg";
 import github from "../../assets/github.png";
 import icoon from "../../assets/icons odf stack.jpg";
@@ -15,8 +22,8 @@ const images = [img1, img2, img3, img4];
 
 function Login() {
 	const [currentIndex, setCurrentIndex] = useState(0);
-	const [snackbarMessage, setSnackbarMessage] = useState('');
-	const [snackbarType, setSnackbarType] = useState('');
+	const [snackbarMessage, setSnackbarMessage] = useState("");
+	const [snackbarType, setSnackbarType] = useState("");
 	const [openSnackbar, setOpenSnackbar] = useState(false);
 
 	useEffect(() => {
@@ -37,21 +44,20 @@ function Login() {
 		setOpenSnackbar(false);
 	};
 	return (
-
-		<Container disableGutters
+		<Container
+			disableGutters
 			maxWidth="100%"
 			sx={{
-
 				overflowY: "hidden",
 				height: "100vh",
 				backgroundColor: "#065AD7",
-
+				overflow: "hidden",
 				display: "flex",
 				padding: "0",
 				margin: "0",
 			}}
-		><MultipleSnackbars
-
+		>
+			<MultipleSnackbars
 				openSnackbar={openSnackbar}
 				snackbarMessage={snackbarMessage}
 				snackbarType={snackbarType}
@@ -59,39 +65,69 @@ function Login() {
 			/>
 			<Stack /* left side body */
 				sx={{
-
 					backgroundColor: "white",
 					width: "40%",
 					justifyContent: "center",
-					alignItems: "center"
+					alignItems: "center",
 				}}
 			>
-				<Stack /*content of left side*/ sx={{ width: "60%", gap: "3rem" }} >
+				<Stack /*content of left side*/ sx={{ width: "60%", gap: "3rem" }}>
 					<Stack /*logo and name*/
-						sx={
-							{
-								width: "auto",
-								display: "flex",
-								flexDirection: "row",
-								alignItems: "center",
-								gap: "1%"
-							}
-						}
-					><Box component="img" height="80%" width="8%" src={icoon} borderRadius="20%"></Box>
-						<Stack /*name*/><Typography variant="h6" fontFamily="Inter" color="#1357B3" fontWeight="bold">Stack Share</Typography></Stack>
+						sx={{
+							width: "auto",
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							gap: "1%",
+						}}
+					>
+						<Box
+							component="img"
+							height="80%"
+							width="8%"
+							src={icoon}
+							borderRadius="20%"
+						></Box>
+						<Stack /*name*/>
+							<Typography
+								variant="h6"
+								fontFamily="Inter"
+								color="#1357B3"
+								fontWeight="bold"
+							>
+								Stack Share
+							</Typography>
+						</Stack>
 					</Stack>
 					<Stack /*login in method*/ sx={{ gap: "5px" }}>
-						<Typography variant="h5" fontFamily="" color="#2E2F39" fontWeight="bold" textAlign="left">Log In to Your Account</Typography>
-						<Typography fontFamily="Inter" color="#A9A9AB" fontSize="70%" fontWeight="bold" textAlign="left">Welcome back! Select method to log In</Typography>
+						<Typography
+							variant="h5"
+							fontFamily=""
+							color="#2E2F39"
+							fontWeight="bold"
+							textAlign="left"
+						>
+							Log In to Your Account
+						</Typography>
+						<Typography
+							fontFamily="Inter"
+							color="#A9A9AB"
+							fontSize="70%"
+							fontWeight="bold"
+							textAlign="left"
+						>
+							Welcome back! Select method to log In
+						</Typography>
 					</Stack>
 					<Stack /*googling and githubing*/
 						sx={{
 							display: "flex",
 							flexDirection: "row",
-							gap: "3%"
-						}}>
-						<Stack /*google*/ sx=
-							{{
+							gap: "3%",
+						}}
+					>
+						<Stack
+							/*google*/ sx={{
 								display: "flex",
 								flexDirection: "row",
 								alignItems: "center",
@@ -101,19 +137,30 @@ function Login() {
 								justifyContent: "center",
 								width: "40%",
 								height: "90%",
-								cursor: "pointer"
+								cursor: "pointer",
 							}}
 							onClick={() => {
 								window.location.href = "http://localhost:7000/auth/google";
 							}}
-
 						>
-							<Stack component="img" sx={{ aspectRatio: 1 }} width="15%" src={google}></Stack>
-							<Typography fontSize="80%" fontFamily="Inter" color="#6e6e67" fontWeight="bold">Google</Typography>
+							<Stack
+								component="img"
+								sx={{ aspectRatio: 1 }}
+								width="15%"
+								src={google}
+							></Stack>
+							<Typography
+								fontSize="80%"
+								fontFamily="Inter"
+								color="#6e6e67"
+								fontWeight="bold"
+							>
+								Google
+							</Typography>
 						</Stack>
 
-						<Stack /*git*/ sx=
-							{{
+						<Stack
+							/*git*/ sx={{
 								display: "flex",
 								flexDirection: "row",
 								alignItems: "center",
@@ -122,51 +169,126 @@ function Login() {
 								borderRadius: "10%",
 								justifyContent: "center",
 								width: "40%",
-								height: "90%"
-							}} onClick={() => handleSnackbar("GitHub login feature is not implemented yet!", "failure")}>
-							<Stack component="img" sx={{ aspectRatio: 1 }} width="15%" src={github}></Stack>
-							<Typography fontSize="80%" fontFamily="Inter" color="#6e6e67" fontWeight="bold">Github</Typography>
+								height: "90%",
+							}}
+							onClick={() =>
+								handleSnackbar(
+									"GitHub login feature is not implemented yet!",
+									"failure",
+								)
+							}
+						>
+							<Stack
+								component="img"
+								sx={{ aspectRatio: 1 }}
+								width="15%"
+								src={github}
+							></Stack>
+							<Typography
+								fontSize="80%"
+								fontFamily="Inter"
+								color="#6e6e67"
+								fontWeight="bold"
+							>
+								Github
+							</Typography>
 						</Stack>
 					</Stack>
 					<Stack /*hr or */>
-						<Box sx={{ display: "flex", alignItems: "center", width: "90%", }}>
-							<Box /* Left line */ sx={{ flex: 1, height: "1px", backgroundColor: "#F6F6F6", }} />
-							<Typography /* Text */ sx={{ marginX: 2, whiteSpace: "nowrap", fontSize: "60%", color: "#BEBFC1" }}>Or Continue with Email</Typography>
-							<Box /* Right line */ sx={{ flex: 1, height: "1px", backgroundColor: "#F6F6F6", }} />
+						<Box sx={{ display: "flex", alignItems: "center", width: "90%" }}>
+							<Box
+								/* Left line */ sx={{
+									flex: 1,
+									height: "1px",
+									backgroundColor: "#F6F6F6",
+								}}
+							/>
+							<Typography
+								/* Text */ sx={{
+									marginX: 2,
+									whiteSpace: "nowrap",
+									fontSize: "60%",
+									color: "#BEBFC1",
+								}}
+							>
+								Or Continue with Email
+							</Typography>
+							<Box
+								/* Right line */ sx={{
+									flex: 1,
+									height: "1px",
+									backgroundColor: "#F6F6F6",
+								}}
+							/>
 						</Box>
 					</Stack>
 					<Stack sx={{ gap: "0.8rem" }}>
-						<Stack /* Usename */ sx={{ border: "2px solid #F9F9F9", width: "80%", display: "flex", flexDirection: "row", alignItems: "center" }} >
+						<Stack
+							/* Usename */ sx={{
+								border: "2px solid #F9F9F9",
+								width: "80%",
+								display: "flex",
+								flexDirection: "row",
+								alignItems: "center",
+							}}
+						>
 							<MdOutlineMail style={{ fontSize: "20px", color: "gray" }} />
-							<TextField placeholder="Email " variant="outlined"
+							<TextField
+								placeholder="Email "
+								variant="outlined"
 								sx={{
-									'& .MuiOutlinedInput-root': {
-										'& fieldset': {
-											border: 'none',
-											width: "100rem"
+									"& .MuiOutlinedInput-root": {
+										"& fieldset": {
+											border: "none",
+											width: "100rem",
 										},
 									},
-								}}></TextField>
+								}}
+							></TextField>
 						</Stack>
-						<Stack /* Password */ sx={{ border: "2px solid #F9F9F9", width: "80%", display: "flex", flexDirection: "row", alignItems: "center" }} >
-							<CiLock
-								style={{ fontSize: "20px", color: "gray" }} />
-							<TextField placeholder="Password" variant="outlined"
+						<Stack
+							/* Password */ sx={{
+								border: "2px solid #F9F9F9",
+								width: "80%",
+								display: "flex",
+								flexDirection: "row",
+								alignItems: "center",
+							}}
+						>
+							<CiLock style={{ fontSize: "20px", color: "gray" }} />
+							<TextField
+								placeholder="Password"
+								variant="outlined"
 								sx={{
-									'& .MuiOutlinedInput-root': {
-										'& fieldset': {
-											border: 'none',
-											width: "100rem"
+									"& .MuiOutlinedInput-root": {
+										"& fieldset": {
+											border: "none",
+											width: "100rem",
 										},
 									},
-								}}></TextField>
+								}}
+							></TextField>
 						</Stack>
-						<Stack /* remember and forget*/ sx={{ width: "80%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
-							<Stack /* Checkbox and remeber me */ sx={{ color: "#E1E5E7" }}><Typography>Remember Me</Typography></Stack>
-							<Stack /* Forgetme */ sx={{ color: "#0759D7" }}><Typography>Forget Password?</Typography></Stack>
+						<Stack
+							/* remember and forget*/ sx={{
+								width: "80%",
+								display: "flex",
+								flexDirection: "row",
+								alignItems: "center",
+								justifyContent: "space-between",
+							}}
+						>
+							<Stack /* Checkbox and remeber me */ sx={{ color: "#E1E5E7" }}>
+								<Typography>Remember Me</Typography>
+							</Stack>
+							<Stack /* Forgetme */ sx={{ color: "#0759D7" }}>
+								<Typography>Forget Password?</Typography>
+							</Stack>
 						</Stack>
 					</Stack>
-					<Button variant="contained" sx={{ width: "80%" }}>Log In </Button>
+					<Button variant="contained" sx={{ width: "80%" }}>
+						Log In{" "}
+					</Button>
 					<Stack
 						sx={{
 							width: "80%",
@@ -174,16 +296,22 @@ function Login() {
 							flexDirection: "row",
 							alignItems: "center",
 							gap: "0.6rem",
-							justifyContent: "center"
-						}}>
-						<Typography>Dont Have An Account ?</Typography><Typography sx={{ color: "#0759D7" }}>Create an Account</Typography></Stack>
+							justifyContent: "center",
+						}}
+					>
+						<Typography>Dont Have An Account ?</Typography>
+						<Typography sx={{ color: "#0759D7" }}>Create an Account</Typography>
+					</Stack>
 				</Stack>
-
 			</Stack>
-			<Stack sx={{
-				width: "60%",
-				alignItems: "center", justifyContent: "center"
-			}}>
+			<Stack
+				sx={{
+					width: "60%",
+					alignItems: "center",
+					justifyContent: "center",
+					height: "100vh",
+				}}
+			>
 				<Box
 					borderRadius="50%"
 					sx={{
@@ -191,8 +319,7 @@ function Login() {
 						width: "50%",
 						overflow: "hidden",
 						backgroundColor: "#f5f5f5",
-						aspectRatio: 1
-
+						aspectRatio: 1,
 					}}
 				>
 					{images.map((image, index) => (
